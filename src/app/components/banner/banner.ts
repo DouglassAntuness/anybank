b1c74df95528408b5dc4d9b5c7019cd42cf3b779
Douglas Antunes <douglasantunesbarreto@gmail.com>
2025-11-02 17:58:35 -0300

    componentes e estilizando o banner

---
+++ src/app/components/banner/banner.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { BoasVindas } from "./boas-vindas/boas-vindas";
+import { Conta } from "./conta/conta";
+
+@Component({
+  selector: 'app-banner',
+  imports: [BoasVindas, Conta],
+  templateUrl: './banner.html',
+  styleUrl: './banner.css',
+})
+export class BannerComponent {
+  dataAtual = new Date();
+
+}
