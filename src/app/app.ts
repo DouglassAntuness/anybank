9d01af09636135deaa10d22353370110a7fc9984
Douglas Antunes <douglasantunesbarreto@gmail.com>
2025-11-02 19:10:33 -0300

    FormsModule

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { BannerComponent } from "./components/banner/banner";
+import { FormNovaTransacao } from './components/form-nova-transacao/form-nova-transacao';
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent],
+  imports: [BannerComponent, FormNovaTransacao],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
