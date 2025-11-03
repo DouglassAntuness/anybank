7aef2e8c25887afce665eaf2368492b18b28184e
Douglas Antunes <douglasantunesbarreto@gmail.com>
2025-11-03 18:58:09 -0300

    input, output, classe, transacoes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { BannerComponent } from "./components/banner/banner";
 import { FormNovaTransacao } from './components/form-nova-transacao/form-nova-transacao';
+import { TipoTransacao, type Transacao } from './models/transacao';
+import { Extrato } from "./components/extrato/extrato";
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent, FormNovaTransacao],
+  imports: [BannerComponent, FormNovaTransacao, Extrato],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('anybank');
+  // assim ele se torna reativo alterando todo o escopo
+  transacoes = signal<Transacao[]>([]);
+  // assim ele é apenas uma variavel
+  // transacoes: Transacao[] = [];
+
+  // computed identifica quando a funcao do callback é alterada, ou seja, quando alterar o this.transacoes vai atualizar o saldo
+  saldo = computed(() => {
+    return this.transacoes().reduce((acc, transacao) => {
+      switch (transacao.tipo) {
+        case TipoTransacao.DEPOSITO:
+          return acc + transacao.valor;
+        case TipoTransacao.SAQUE:
+          return acc - transacao.valor
+        default:
+          throw new Error("Tipo de transação identificado");
+      }
+    }, 0);
+  });
+
+  processarTransacao(transacao: Transacao) {
+    if(transacao.tipo == TipoTransacao.SAQUE && transacao.valor > this.saldo()) return alert("Saldo indisponível");
+    this.transacoes.update((listaAtual) => [transacao, ...listaAtual]);
+  }
 }
--- src/app/components/form-nova-transacao/form-nova-transacao.ts
+++ src/app/components/form-nova-transacao/form-nova-transacao.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TipoTransacao, Transacao } from '../../models/transacao';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [FormsModule],
+  imports: [FormsModule, KeyValuePipe],
   templateUrl: './form-nova-transacao.html',
   styleUrl: './form-nova-transacao.css',
 })
@@ -11,7 +13,19 @@ export class FormNovaTransacao {
   tipoTransacao = "";
   valorTransacao = "";
 
-  handleSalvarTransacao() {
+  transacaoCriada = output<Transacao>();
 
+  tipoTransacaoEnum = TipoTransacao;
+
+  handleSalvarTransacao() {
+    const transacao = new Transacao(
+      this.tipoTransacao as TipoTransacao,
+      Number(this.valorTransacao)
+    );
+
+    this.transacaoCriada.emit(transacao);
+
+    this.tipoTransacao = "";
+    this.valorTransacao = "";
   }
 }
